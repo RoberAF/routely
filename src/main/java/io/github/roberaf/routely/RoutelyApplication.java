b345ffd1a93db13a4835ac800476591dcf6ee11e
Roberto Álvarez Ferrera <ralvarezferrera@gmail.com>
2026-07-15 21:31:33 +0200

    bootstrap spring boot 4 project skeleton

---
+++ src/main/java/io/github/roberaf/routely/RoutelyApplication.java
@@ -0,0 +1,13 @@
+package io.github.roberaf.routely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutelyApplication.class, args);
+	}
+
+}
